a14bf09b6b62465962d85d0867dee7f5bb48f280
Ashutosh Pratap Singh <84658340+singhashutosh01@users.noreply.github.com>
2021-06-22 19:21:14 +0530

    Create recursiveDFS.cpp

---
+++ recursiveDFS.cpp
@@ -0,0 +1,10 @@
+vector <int> adj[10];
+    bool visited[10];
+
+    void dfs(int s) {
+        visited[s] = true;
+        for(int i = 0;i < adj[s].size();++i)    {
+         if(visited[adj[s][i]] == false)
+             dfs(adj[s][i]);
+        }
+    }
