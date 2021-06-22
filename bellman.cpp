803675bc8946b95a90bd6128aa81a37bdf034263
Ashutosh Pratap Singh <84658340+singhashutosh01@users.noreply.github.com>
2021-06-22 19:30:12 +0530

    Create bellman.cpp

---
+++ bellman.cpp
@@ -0,0 +1,9 @@
+int dist[N], v, w;
+
+cl(dist,63);
+dist[0] = 0;
+for (int i = 0; i < n-1; ++i)
+  for (int u = 0; u < n; ++u)
+    for (int j = 0; j < adj[u].size(); ++j)
+      v = adj[u][j], w = adjw[u][j],
+      dist[v] = min(dist[v], dist[u]+w);
