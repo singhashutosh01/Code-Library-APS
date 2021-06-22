a0ae6f765cea755f7b8fd83c0d8657b8cdb62305
Ashutosh Pratap Singh <84658340+singhashutosh01@users.noreply.github.com>
2021-06-22 19:27:27 +0530

    Create tries.cpp

---
+++ tries.cpp
@@ -0,0 +1,11 @@
+int trie[N][26], trien = 1;
+
+int add(int u, char c){
+  c-='a';
+  if (trie[u][c]) return trie[u][c];
+  return trie[u][c] = ++trien;
+}
+
+//to add a string s in the trie
+int u = 1;
+for(char c : s) u = add(u, c);
